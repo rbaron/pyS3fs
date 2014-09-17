549b52e308e57a108bab79f3aa9b51a2ff81107f
Raphael Baron <rbaron@rbaron.net>
2014-09-17 10:18:59 -0300

    Moved version to 0.2.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
       name="pyS3fs",
       description="Mount Amazon's S3 bucket as a filesystem",
-      version="0.1",
+      version="0.2",
       url="http://github.com/rbaron/pyS3fs",
       install_requires=["fusepy", "boto"],
       packages=find_packages(),
